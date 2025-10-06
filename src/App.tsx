ed3c46cbaa7139825595e323d844b64b1d689cd2
Victor Terres <vterres7@gmail.com>
2025-10-06 14:34:54 -0400

    new components and organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
+import { Heading } from './components/Heading/Heading';
+import { Container } from './components/Container/Container';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
-    <div>
-      <Heading />
-      <div>Olá Mundo</div>
-    </div>
+    <>
+      <Container>
+        <section>
+          <Heading>
+            <TimerIcon />
+          </Heading>
+        </section>
+      </Container>
+
+      <Container>
+        <section>Menu</section>
+      </Container>
+
+      <Container>
+        <section>Counter</section>
+      </Container>
+
+      <Container>
+        <section>Footer</section>
+      </Container>
+    </>
   );
 }
